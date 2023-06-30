07cd5e0a58de2291cada6fbf09b47b5120ccec70
nauht <vthuan26655@gmail.com>
2023-06-30 12:04:33 +0700

    upgrade wake backend up function

--- next.config.js
+++ next.config.js
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
 const cron = require('node-cron');
-cron.schedule('*/5 * * * *', () => {
+cron.schedule('*/2 * * * *', () => {
+    function makeid(length) {
+        let result = '';
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        let counter = 0;
+        while (counter < length) {
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+          counter += 1;
+        }
+        return result;
+    }
   const pokeBackend = async () => {
         console.log("poke backend");
+        const randSearch = makeid(2)
         const result = await fetch(
-            "https://botzone.onrender.com/" + `api/product`
+            "https://botzone.onrender.com/" + `api/product/search/${randSearch}`
         );
         const data = await result.json();
+        console.log(data);
     };
     pokeBackend()
 });
